02b2d8ffd78a3ddcd693f0d991bd4c5d0ef54c80
moenova <harryshanghai190@gmail.com>
2019-09-21 01:03:41 +0800

    Add files via upload

--- start.py
+++ start.py
@@ -4,4 +4,5 @@ from src.memory import startTable,startNode
 #startTable("genki test","res/","eng","hiragana")
 #startNode("test","res/","第一章节")
 
-startTable("genki 2 volcab","res/","eng","hiragana",lesson=13)
+#startTable("genki 2 volcab","res/","eng","hiragana",lesson=13)
+startTable("genki 2 kanji","res/","hiragana","kanji",lesson=13)
